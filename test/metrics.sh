657a3777b7825e86ba5d4f37ec5a9fc1d9f373f2
rsethur <rsethur@gmail.com>
2021-07-17 08:14:48 -0700

    test scripts

---
+++ test/metrics.sh
@@ -0,0 +1,16 @@
+#example cli syntax for test purposes
+#endpoint
+az monitor metrics list-definitions --resource /subscriptions/5f08d643-1910-4a38-a7c7-84a39d4f42e0/resourceGroups/sethurg/providers/Microsoft.MachineLearningServices/workspaces/sethucanary/onlineEndpoints/demo-endpoint
+az monitor metrics list --metric RequestLatency_P95 --resource /subscriptions/5f08d643-1910-4a38-a7c7-84a39d4f42e0/resourceGroups/sethurg/providers/Microsoft.MachineLearningServices/workspaces/sethucanary/onlineEndpoints/demo-endpoint
+az monitor metrics list --metric RequestsPerMinute --filter "deployment eq 'green' and statusCodeClass ne '2xx'" --aggregation Average --interval PT1M --start-time 02/21/2021T21:52:00+00:00 --end-time 02/21/2021T22:00:00+00:00 --resource /subscriptions/5f08d643-1910-4a38-a7c7-84a39d4f42e0/resourceGroups/sethurg/providers/Microsoft.MachineLearningServices/workspaces/sethucanary/onlineEndpoints/demo-endpoint
+
+#deployment
+az monitor metrics list-definitions --resource  /subscriptions/5f08d643-1910-4a38-a7c7-84a39d4f42e0/resourceGroups/sethurg/providers/Microsoft.MachineLearningServices/workspaces/sethucanary/onlineEndpoints/demo-endpoint/deployments/blue
+az monitor metrics list --metric "MemoryUtilization" --aggregation Average --interval PT1M --start-time 2021-03-21T14:18:00+00:00 --end-time 2021-03-21T14:21:00+00:00 --resource /subscriptions/5f08d643-1910-4a38-a7c7-84a39d4f42e0/resourceGroups/sethurg/providers/Microsoft.MachineLearningServices/workspaces/sethucanary/onlineEndpoints/demo-endpoint/deployments/blue
+az monitor metrics list --metric "CPUUtilization" --aggregation Average --interval PT1M --start-time 2021-03-21T14:18:00+00:00 --end-time 2021-03-21T14:21:00+00:00 --resource /subscriptions/5f08d643-1910-4a38-a7c7-84a39d4f42e0/resourceGroups/sethurg/providers/Microsoft.MachineLearningServices/workspaces/sethucanary/onlineEndpoints/demo-endpoint/deployments/blue
+
+az monitor metrics list --metric "MemoryUtilization" --aggregation Average --interval PT1M --start-time 2021-07-16T14:18:00+00:00 --end-time 2021-07-16T14:21:00+00:00 --resource /subscriptions/6560575d-fa06-4e7d-95fb-f962e74efd7a/resourceGroups/saferollout/providers/Microsoft.MachineLearningServices/workspaces/saferollout/onlineEndpoints/gpt-2/deployments/blue
+
+
+#using validate_metrics script
+python -m test.validate_metrics --chart_name "Validate memory within threshold" --chart_save_path "chart-output" --metric MemoryUtilization --aggregation Average --metrics_condition lte --interval PT1M --num_intervals 4 --threshold 30 --resource_id /subscriptions/6560575d-fa06-4e7d-95fb-f962e74efd7a/resourceGroups/saferollout/providers/Microsoft.MachineLearningServices/workspaces/saferollout/onlineEndpoints/gpt-2/deployments/blue
