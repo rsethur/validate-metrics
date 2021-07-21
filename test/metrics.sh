d792f1440e155045f592c3579ef027e7b4e39f14
rsethur <rsethur@gmail.com>
2021-07-20 17:37:11 -0700

    fix

--- test/metrics.sh
+++ test/metrics.sh
@@ -13,4 +13,4 @@ az monitor metrics list --metric "MemoryUtilization" --aggregation Average --int
 
 
 #using validate_metrics script
-python -m test.validate_metrics --chart_name "Validate memory within threshold" --chart_save_path "chart-output" --metric MemoryUtilization --aggregation Average --metrics_condition lte --interval PT1M --num_intervals 4 --threshold 30 --resource_id /subscriptions/6560575d-fa06-4e7d-95fb-f962e74efd7a/resourceGroups/saferollout/providers/Microsoft.MachineLearningServices/workspaces/saferollout/onlineEndpoints/gpt-2/deployments/blue
+python -m test.validate_metrics --client_secret --tenant_id "72f988bf-86f1-41af-91ab-2d7cd011db47" --client_id "9aee0ba6-c30b-4c50-ab28-068ba932cd6b" --chart_name "Validate memory within threshold" --chart_save_path "chart-output" --metric MemoryUtilization --aggregation Average --metrics_condition lte --interval PT1M --num_intervals 4 --threshold 30 --resource_id /subscriptions/6560575d-fa06-4e7d-95fb-f962e74efd7a/resourceGroups/saferollout/providers/Microsoft.MachineLearningServices/workspaces/saferollout/onlineEndpoints/gpt-2/deployments/blue
