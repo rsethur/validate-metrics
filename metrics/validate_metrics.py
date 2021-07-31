d9abc08b61d7dfccba9ad655631a9abf4fc83107
rsethur <rsethur@gmail.com>
2021-07-30 20:03:35 -0700

    fix

--- metrics/validate_metrics.py
+++ metrics/validate_metrics.py
@@ -114,6 +114,7 @@ def validate_metrics(metric, aggregation, filter, interval, threshold, metrics_c
             if chart_name is not None:
                 if not os.path.exists(chart_save_path):
                     os.makedirs(chart_save_path)
+                logging.info("***chart path: "+ chart_save_path)
                 plt.savefig(os.path.join(chart_save_path, chart_name))
     if threshold_breached:
         raise Exception("Threshold breached!")
